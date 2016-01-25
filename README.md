4f57cc41d2d53712bb4b89fdbb54afe815eef359
shelan <shelanrc@gmail.com>
2016-01-25 22:52:48 +0100

    Adding commands to test docker api

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # DockerJcloudsTest
 
 This is a sample for running docker with Jclouds API
+
+### To get the id of the image
+docker images --no-trunc
+
+### To stop all the docker images
+docker stop $(docker ps -a -q)
+docker rm $(docker ps -a -q)
--- src/main/java/kth/se/DockerLauncher.java
+++ src/main/java/kth/se/DockerLauncher.java
@@ -12,6 +12,7 @@ import org.jclouds.compute.domain.Template;
 import org.jclouds.logging.log4j.config.Log4JLoggingModule;
 import org.jclouds.sshj.config.SshjSshClientModule;
 
+import java.util.Properties;
 import java.util.Set;
 
 /**
@@ -24,18 +25,24 @@ public class DockerLauncher {
     public static void main(String[] args) throws RunNodesException {
 
 
-        String email = "";
-        String password = "";
+        String email = "/Users/shelan/.docker/machine/certs/cert.pem";
+        String password = "/Users/shelan/.docker/machine/certs/key.pem";
 
         // get a context with docker that offers the portable ComputeService api
-        ComputeServiceContext context = ContextBuilder.newBuilder("docker")
+        ContextBuilder contextBuilder = ContextBuilder.newBuilder("docker").endpoint("https://192.168.99.100:2376")
                 .credentials(email, password)
                 .modules(ImmutableSet.<Module>of(new Log4JLoggingModule(),
-                        new SshjSshClientModule()))
-                .buildView(ComputeServiceContext.class);
+                        new SshjSshClientModule()));
+
+        Properties props = new Properties();
+
+        props.setProperty("docker.cacert.path","/Users/shelan/.docker/machine/certs/ca.pem");
+        contextBuilder.overrides(props);
+
+        ComputeServiceContext context = contextBuilder.buildView(ComputeServiceContext.class);
         ComputeService client = context.getComputeService();
 
-        String sshableImageId = "your-sshable-image-id"; // this can be obtained using `docker images --no-trunc` command
+        String sshableImageId = "dfd70f9dcca4091b8cafb7614af6389bc3d9b47051867b4790c46ea81bd7493f"; // this can be obtained using `docker images --no-trunc` command
         Template template = client.templateBuilder().imageId(sshableImageId).build();
 
 // run a couple nodes accessible via group container
